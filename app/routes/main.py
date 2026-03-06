7e8ca175f5559513c6a56f6b79e74c245383851d
rushirajsinh zala <rushirajsinhzala16@gmail.com>
2026-03-06 16:25:48 +0530

    movie & tv detail page

--- app/routes/main.py
+++ app/routes/main.py
@@ -2,7 +2,7 @@ from typing import List
 
 from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
-from themoviedb import TMDb, Genre
+from themoviedb import TMDb, Genre, PartialMovie
 
 from ..models import Movie, Rating, db
 
@@ -11,23 +11,34 @@ from ..models import Movie, Rating, db
 
 main = Blueprint('main', __name__)
 
+tmdb = TMDb()
 
 @main.route('/')
 @login_required
 def index():
     # 1. Fetch Movies by Genre for the Netflix Rows
     # Make sure these genres match what you loaded in scripts/load_tmdb.py
-    genres = ["Action", "Comedy", "Drama", "Sci-Fi"]
+    # genres = ["Action", "Comedy", "Drama", "Sci-Fi"]
 
     # We create a dictionary where each key is a genre and the value is a list of movies
-    genre_data = {
-        g: Movie.query.filter(Movie.genre.ilike(f'%{g}%')).limit(10).all()
-        for g in genres
-    }
+    # genre_data = {
+    #     g: Movie.query.filter(Movie.genre.ilike(f'%{g}%')).limit(10).all()
+    #     for g in genres
+    # }
+    genre_data:dict[str,  list[PartialMovie] | None] = {}
+
+    genre_data['Action'] = tmdb.discover().movie(page=1, with_genres='28').results
+    genre_data['Comedy'] = tmdb.discover().movie(page=1, with_genres='35').results
+    genre_data['Drama'] = tmdb.discover().movie(page=1, with_genres='18').results
+    genre_data['Sci-Fi'] = tmdb.discover().movie(page=1, with_genres='878').results
 
     # 2. Pick a "Featured" movie for the Hero/Top Banner
-    featured = Movie.query.order_by(Movie.tmdb_rating.desc()).first()
-
+    # featured = Movie.query.order_by(Movie.tmdb_rating.desc()).first()
+    data_featured = tmdb.movies().top_rated().results
+    if len(data_featured) > 0:
+        featured = data_featured[0]
+    else:
+        featured = None
     # 3. Render the Netflix-style template
     return render_template('index.html', genre_data=genre_data, featured=featured)
 
@@ -58,7 +69,6 @@ def search():
     if not query:
         return redirect(url_for('main.index'))
 
-    tmdb = TMDb()
     # Search for movies where the title contains the query (case-insensitive)
     movies_record = tmdb.search().movies(query=query).results
     # Movie.query.filter(Movie.title.ilike(f'%{query}%')).all()
@@ -87,7 +97,6 @@ def movie_detail_page(movie_id:int):
     if movie_id <= 0:
         return redirect(url_for('main.index'))
 
-    tmdb = TMDb()
     tmdb_movie = tmdb.movie(movie_id)
     movie_detail = tmdb_movie.details()
 
@@ -107,4 +116,32 @@ def movie_detail_page(movie_id:int):
                            movie_similar=movie_similar,
                            movie_credits=movie_credits,
                            movie_images=movie_images
+                           )
+
+
+@main.route('/tv-show/<int:tv_show_id>')
+@login_required
+def tv_show_detail_page(tv_show_id:int):
+    if tv_show_id <= 0:
+        return redirect(url_for('main.index'))
+
+    tmdb_tv_show = tmdb.tv(tv_show_id)
+    tv_show_detail = tmdb_tv_show.details()
+
+    if tv_show_detail is None:
+        return redirect(url_for('main.index'))
+
+    tv_show_recommendations = tmdb_tv_show.recommendations().results
+    tv_show_similar = tmdb_tv_show.similar().results
+    tv_show_videos = tmdb_tv_show.videos().results
+    tv_show_credits = tmdb_tv_show.credits()
+    tv_show_images = tmdb_tv_show.images()
+
+    return render_template('tv_show_detail.html',
+                           tv_show_detail=tv_show_detail,
+                           tv_show_videos=tv_show_videos,
+                           tv_show_recommendations=tv_show_recommendations,
+                           tv_show_similar=tv_show_similar,
+                           tv_show_credits=tv_show_credits,
+                           tv_show_images=tv_show_images
                            )
